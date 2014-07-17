e494d53075cfd0de1fa029caa13c6d7151285f04
antonioricardojr <antonioricardojr@gmail.com>
2014-07-17 16:55:52 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NciSummerGiftcheaters::Application.config.secret_token = '2d9fcb95e1d2ce7a1e661997550053f81325b01e3f92494f38b610c241064a1f19bdeb08ac04d80b7c14476c09a8474d86adffb9bfefba49faf96962fd6cba91'
