4a87d59c47bdb80f27b5405ec9d641c4e87fe4f3
antonioricardojr <antonioricardojr@gmail.com>
2014-07-26 07:05:25 +0100

    trying to dix the deployed version

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,4 +3,4 @@
 #SITE_URL = 'http://localhost:3000/'
 APP_ID= '309428675901394'
 APP_SECRET= 'bb93a431aa2c57f02f93b6a3a0d7c38b'
-SITE_URL = 'http://giftcheaters.herokuapp.com/'
+SITE_URL = 'http://giftcheaters.herokuapp.com'
