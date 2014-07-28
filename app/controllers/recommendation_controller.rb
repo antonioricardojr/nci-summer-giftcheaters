7867bdc05e5b58087bb054fef74fd7bb9d8c8850
Malu <malumatos@gmail.com>
2014-07-28 16:34:47 +0100

    Pie chart clickable

--- app/controllers/recommendation_controller.rb
+++ app/controllers/recommendation_controller.rb
@@ -8,14 +8,15 @@ class RecommendationController < ApplicationController
 		@friend_profile = @api.get_object(params[:id_friend])
 		
 		@friend_likes = @api.get_connections(@friend_profile["id"], "likes")
-		@friend_likes_filtered = filter_likes(@friend_likes)
+		@friend_likes_filtered, @categories, @links_categories = filter_likes(@friend_likes)
 	end
 
 
 	def filter_likes(friend_likes)
 
 		filtered_likes = []
-
+		categories = {}
+		links_categories = {}
 		
 		friend_likes.each do |like_1|
 			category = []
@@ -30,13 +31,13 @@ class RecommendationController < ApplicationController
 
 				end			
 			end
-
-
-
 			like_filtered = category[rand(category.length)]
 			filtered_likes.push(like_filtered) 
+			categories[like_filtered['category']] = category.length
+			link = '/search?param1=' + like_filtered['category'] + '&param2=' + like_filtered['name'].to_param
+			links_categories[like_filtered['category']] = link
 		end
-		return filtered_likes
+		return filtered_likes, categories, links_categories
 	end
 
 end
